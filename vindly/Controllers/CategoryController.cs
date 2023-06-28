dfeed48b71d44b47462ecd6c3b063c81af14ef3a
mrwaipyae <waipyae.code@gmail.com>
2023-06-28 15:41:19 +0630

    update filter and export

--- vindly/Controllers/CategoryController.cs
+++ vindly/Controllers/CategoryController.cs
@@ -26,6 +26,7 @@ namespace vindly.Controllers
             return View();
         }
 
+        
         public IActionResult Filter(string query)
         {
             IEnumerable<Category> filteredData;
@@ -70,6 +71,50 @@ namespace vindly.Controllers
 
         
         }
+        
+
+
+        /***
+        public IActionResult Filter(string query)
+        {
+            IQueryable<Category> filteredData;
+
+            if (query == "all")
+            {
+                // No specific query selected, show all data
+                filteredData = _db.Category;
+            }
+            else if (query == "pmrd")
+            {
+                // Filter the data based on the 'PM/RD' query
+                filteredData = _db.Category.Where(category => category.PM > 0 || category.RD > 0);
+            }
+            else if (query == "utot")
+            {
+                // Filter the data based on the 'UT/OT' query
+                filteredData = _db.Category.Where(category => category.UT > 0 || category.OT > 0);
+            }
+            else
+            {
+                // Invalid query parameter, show no data
+                filteredData = Enumerable.Empty<Category>().AsQueryable();
+            }
+
+            // Pass the filtered data to your partial view
+            var filteredCategories = filteredData.Select(category => new Category
+            {
+                Id = category.Id,
+                Name = category.Name,
+                PM = category.PM,
+                RD = category.RD,
+                UT = category.UT,
+                OT = category.OT
+            }).ToList();
+
+            return PartialView("_CategoryPartialView", filteredCategories);
+        }
+
+        ***/
 
     }
 }
